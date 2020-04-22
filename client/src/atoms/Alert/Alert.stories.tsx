7b69294cc3e96d6c1f77c3b65362352eea0292ce
Chris Dixon <chris@welfordmedia.co.uk>
2020-04-22 18:03:45 +0100

    type

--- client/src/atoms/Alert/Alert.stories.tsx
+++ client/src/atoms/Alert/Alert.stories.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Alert from './Alert';
+import React from 'react'
+import Alert from './Alert'
 import ProviderWrapper from 'utils/ProviderWrapper'
 import { SchemaConst } from 'utils/baseSchema'
 import { AlertTypeConst } from 'atoms/Alert/alertType'
@@ -10,36 +10,95 @@ import AlertMessage from 'atoms/Alert/AlertMessage'
 export default {
   title: 'Alert',
   component: Alert,
-};
+}
 
 const successStore = [
-  { key: SchemaConst.ALERT, value: {alertType: AlertTypeConst.SUCCESS, message: 'Success Alert'} },
+  {
+    key: SchemaConst.ALERT,
+    value: { alertType: AlertTypeConst.SUCCESS, message: 'Success Alert' },
+  },
 ]
 
 const warningStore = [
-  { key: SchemaConst.ALERT, value: {alertType: AlertTypeConst.WARNING, message: 'Warning Alert'} },
+  {
+    key: SchemaConst.ALERT,
+    value: { alertType: AlertTypeConst.WARNING, message: 'Warning Alert' },
+  },
 ]
 
 const infoStore = [
-  { key: SchemaConst.ALERT, value: {alertType: AlertTypeConst.INFO, message: 'Info Alert'} },
+  {
+    key: SchemaConst.ALERT,
+    value: { alertType: AlertTypeConst.INFO, message: 'Info Alert' },
+  },
 ]
 
 const errorStore = [
-  { key: SchemaConst.ALERT, value: {alertType: AlertTypeConst.ERROR, message: 'Error Alert'} },
+  {
+    key: SchemaConst.ALERT,
+    value: { alertType: AlertTypeConst.ERROR, message: 'Error Alert' },
+  },
 ]
 
-export const SuccessAlert = () => <ProviderWrapper store={successStore}><Alert/></ProviderWrapper>
-export const WarningAlert = () => <ProviderWrapper store={warningStore}><Alert/></ProviderWrapper>
-export const InfoAlert = () => <ProviderWrapper store={infoStore}><Alert/></ProviderWrapper>
-export const ErrorAlert = () => <ProviderWrapper store={errorStore}><Alert/></ProviderWrapper>
+export const SuccessAlert = () => (
+  <ProviderWrapper store={successStore}>
+    <Alert />
+  </ProviderWrapper>
+)
+export const WarningAlert = () => (
+  <ProviderWrapper store={warningStore}>
+    <Alert />
+  </ProviderWrapper>
+)
+export const InfoAlert = () => (
+  <ProviderWrapper store={infoStore}>
+    <Alert />
+  </ProviderWrapper>
+)
+export const ErrorAlert = () => (
+  <ProviderWrapper store={errorStore}>
+    <Alert />
+  </ProviderWrapper>
+)
 
+export const SuccessContent = () => (
+  <AlertContent
+    variant={AlertTypeConst.SUCCESS}
+    message={'Success Content'}
+    onClose={action('close')}
+  />
+)
+export const WarningContent = () => (
+  <AlertContent
+    variant={AlertTypeConst.WARNING}
+    message={'Warning Content'}
+    onClose={action('close')}
+  />
+)
+export const InfoContent = () => (
+  <AlertContent
+    variant={AlertTypeConst.INFO}
+    message={'Info Content'}
+    onClose={action('close')}
+  />
+)
+export const ErrorContent = () => (
+  <AlertContent
+    variant={AlertTypeConst.ERROR}
+    message={'Error Content'}
+    onClose={action('close')}
+  />
+)
 
-export const SuccessContent = () => <AlertContent variant={AlertTypeConst.SUCCESS} message={'Success Content'} onClose={action('close')} />
-export const WarningContent = () => <AlertContent variant={AlertTypeConst.WARNING} message={'Warning Content'} onClose={action('close')} />
-export const InfoContent = () => <AlertContent variant={AlertTypeConst.INFO} message={'Info Content'} onClose={action('close')} />
-export const ErrorContent = () => <AlertContent variant={AlertTypeConst.ERROR} message={'Error Content'} onClose={action('close')} />
-
-export const SuccessMessage = () => <AlertMessage variant={AlertTypeConst.SUCCESS} message={'Success Message'} onClose={action('close')} />
-export const WarningMessage = () => <AlertMessage variant={AlertTypeConst.WARNING} message={'Warning Message'} onClose={action('close')} />
-export const InfoMessage = () => <AlertMessage variant={AlertTypeConst.INFO} message={'Info Message'} onClose={action('close')} />
-export const ErrorMessage = () => <AlertMessage variant={AlertTypeConst.ERROR} message={'Error Message'} onClose={action('close')} />
+export const SuccessMessage = () => (
+  <AlertMessage variant={AlertTypeConst.SUCCESS} message={'Success Message'} />
+)
+export const WarningMessage = () => (
+  <AlertMessage variant={AlertTypeConst.WARNING} message={'Warning Message'} />
+)
+export const InfoMessage = () => (
+  <AlertMessage variant={AlertTypeConst.INFO} message={'Info Message'} />
+)
+export const ErrorMessage = () => (
+  <AlertMessage variant={AlertTypeConst.ERROR} message={'Error Message'} />
+)
