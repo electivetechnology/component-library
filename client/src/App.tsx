0f46c680ced691c3c3069ef547b5082936976a5b
Chris Dixon <chris@welfordmedia.co.uk>
2020-04-22 12:53:31 +0100

    tests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App: FunctionComponent = () => {
   return (
-    <div className="App">
+    <div className="App" data-testid="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
