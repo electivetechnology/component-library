b1fb7f1d9b03b5b6790a775faac7aa0327959b9e
Chris Dixon <chris@welfordmedia.co.uk>
2020-04-24 21:21:55 +0100

    auto phone

---
+++ client/src/components/atoms/PhoneInput/PhoneInput.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PhoneInput from './PhoneInput';
+
+export default {
+  title: 'atoms/PhoneInput',
+  component: PhoneInput,
+};
+
+export const Default = () => <PhoneInput />;
