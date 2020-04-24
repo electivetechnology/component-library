8c6e5fcaf97f77a9983b8213690c570bc82434c5
Chris Dixon <chris@welfordmedia.co.uk>
2020-04-24 20:25:00 +0100

    loader

---
+++ client/src/components/atoms/Loader/Loader.tsx
@@ -0,0 +1,16 @@
+import React  from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import { Wrapper, LinearProgressWithStyled } from './styles'
+
+const LinearProgressStyled = withStyles(LinearProgressWithStyled)(LinearProgress)
+
+const Loader = () => {
+  return (
+    <Wrapper data-testid="Loader">
+      <LinearProgressStyled />
+    </Wrapper>
+  )
+}
+
+export default Loader
---
+++ client/src/components/atoms/Loader/styles.tsx
@@ -0,0 +1,15 @@
+import { theme } from 'styles/theme'
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  width: 100%;
+`
+
+export const LinearProgressWithStyled =  {
+  colorPrimary: {
+    backgroundColor: theme.primaryColorValenciaLight,
+  },
+  barColorPrimary: {
+    backgroundColor: theme.primaryColorValencia,
+  },
+}
