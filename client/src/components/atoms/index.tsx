d67883bf8bdc5d47fdccad369c2ac2625c66048a
Chris Dixon <chris@welfordmedia.co.uk>
2020-06-16 19:13:15 +0100

    line

--- client/src/components/atoms/index.tsx
+++ client/src/components/atoms/index.tsx
@@ -3,4 +3,3 @@ export { default as Button } from './Button/Button'
 export { default as CloseButton } from './CloseButton/CloseButton'
 export { default as Checkbox } from './Checkbox/Checkbox'
 export { default as CsvDownloader } from './CsvDownloader/CsvDownloader'
-
