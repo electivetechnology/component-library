7938a09aef8744bd5cb876edb2e1238e435e6029
Chris Dixon <chris@welfordmedia.co.uk>
2020-06-16 19:06:44 +0100

    index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,10 @@
-import * as React from 'react'
+// atoms
+export { Avatar } from 'components/atoms'
+export { Button } from 'components/atoms'
+export { CloseButton } from 'components/atoms'
+export { Checkbox } from 'components/atoms'
+export { CsvDownloader } from 'components/atoms'
 
-interface Props {
-  text: string
-}
-
-export const ExampleComponent = ({ text }: Props) => {
-  return <div>Example Component: {text}</div>
-}
-
-export { Button } from './components/atoms'
-
+// molecules
+export { Alert } from 'components/molecules'
+export { Channels } from 'components/molecules'
