ff617c90f73db47aad0de91d92e4aac8ff45855e
Chris Dixon <chris@welfordmedia.co.uk>
2020-06-18 13:58:23 +0100

    test

--- client/src/components/molecules/PopUp/PopUp.test.tsx
+++ client/src/components/molecules/PopUp/PopUp.test.tsx
@@ -3,12 +3,17 @@ import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import PopUp from './PopUp'
 import ProviderWrapper from 'utils/ProviderWrapper'
-import { refreshStore } from './mocks'
+import { noCloseStore, closeStore } from './mocks'
 
 afterEach(cleanup)
 
-test('renders PopUp', () => {
-  const { getByTestId } = render(<ProviderWrapper store={refreshStore}><PopUp/></ProviderWrapper>)
+test('renders PopUp no close', () => {
+  const { getByTestId } = render(<ProviderWrapper store={noCloseStore}><PopUp/></ProviderWrapper>)
+  expect(getByTestId('PopUp')).toBeDefined
+})
+
+test('renders PopUp close', () => {
+  const { getByTestId } = render(<ProviderWrapper store={closeStore}><PopUp/></ProviderWrapper>)
   expect(getByTestId('PopUp')).toBeDefined
 })
 
