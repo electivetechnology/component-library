67300901d3bf9b2c542a86e16071adb75b960292
Chris Dixon <chris@welfordmedia.co.uk>
2020-11-06 20:33:28 +0000

    latest

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,11 +1,7 @@
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs',
-    '@storybook/addon-backgrounds/register',
-    '@storybook/addon-viewport/register'
-  ],
-};
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
