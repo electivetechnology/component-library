18692e47a0f18288c3cf9eafe91a87150d4b2119
Chris Dixon <chris.js.dixon@gmail.com>
2020-12-12 14:43:15 +0000

    action

--- client/src/components/organisms/Form/index.tsx
+++ client/src/components/organisms/Form/index.tsx
@@ -3,5 +3,6 @@ import FormInput from './FormInput'
 import FormRow from './FormRow'
 import FormTitle from './FormTitle'
 import FormInfo from './FormInfo'
+import FormAction from './FormAction'
 
-export { Form, FormInput, FormRow, FormTitle, FormInfo }
+export { Form, FormInput, FormRow, FormTitle, FormInfo, FormAction }
