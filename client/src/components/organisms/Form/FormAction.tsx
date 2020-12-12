7930a8199f8c727564b57ad91ee46b63fb29ff49
Chris Dixon <chris.js.dixon@gmail.com>
2020-12-12 14:43:15 +0000

    action

---
+++ client/src/components/organisms/Form/FormAction.tsx
@@ -0,0 +1,24 @@
+import React, { FunctionComponent, memo, ReactElement } from 'react'
+import { ActionTextStyled } from './styles'
+import { Font } from 'components/atoms'
+import { SvgIconProps } from '@material-ui/core/SvgIcon'
+
+interface Props {
+  label: string
+  handleAction: any
+  icon?: ReactElement<SvgIconProps>
+}
+const FormAction: FunctionComponent<Props> = ({
+  label,
+  handleAction,
+  icon
+}) => {
+  return (
+    <ActionTextStyled data-testid='FormAction' onClick={handleAction}>
+      <Font variant={'h6'}>{label}</Font>
+      {icon && icon}
+    </ActionTextStyled>
+  )
+}
+
+export default memo(FormAction)
