63047711a606a0001d0ccddb4c397bf01a956949
Chris Dixon <chris.js.dixon@gmail.com>
2020-12-12 18:52:14 +0000

    story

--- client/src/components/organisms/Form/Form.stories.tsx
+++ client/src/components/organisms/Form/Form.stories.tsx
@@ -1,5 +1,9 @@
-import React  from 'react'
-import { Form, FormInput, FormRow } from 'components/organisms/Form'
+import React from 'react'
+import {
+  Form,
+  FormInput,
+  FormRow,
+} from 'components/organisms/Form'
 
 const handleUpdate = (formData: any) => {
   console.group('handleUpdate')
@@ -9,45 +13,23 @@ const handleUpdate = (formData: any) => {
 
 const Template = (args: any) => <Form {...args} handleUpdate={handleUpdate} />
 
-// Form
+// InputSingle
 export const InputSingle: any = Template.bind({})
 
 InputSingle.args = {
   handleUpdate,
-  children: (
-    <FormInput
-      input={{
-        label: 'Name',
-        value: 'some value',
-        type: 'text',
-        required: true
-      }}
-    />
-  )
+  children: <FormInput label='Name' name='name' value='some name' type='text' />
 }
 
+// InputRow
 export const InputRow: any = Template.bind({})
 
 InputRow.args = {
   handleUpdate,
   children: (
     <FormRow>
-      <FormInput
-        input={{
-          label: 'Name',
-          value: 'some name',
-          type: 'text',
-          required: true
-        }}
-      />
-      <FormInput
-        input={{
-          label: 'Desc',
-          value: 'some desc',
-          type: 'text',
-          required: true
-        }}
-      />
+      <FormInput label='Name' name='name' value='some name' type='text' />
+      <FormInput label='Desc' name='desc' value='some desc' type='text' />
     </FormRow>
   )
 }
