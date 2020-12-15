37f085070956fa8791bc12f265bce5d00738d214
Chris Dixon <chris.js.dixon@gmail.com>
2020-12-15 14:28:15 +0000

    component

--- client/src/components/atoms/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as Avatar } from './Avatar/Avatar'
-export { default as Button } from './Button/Button'
-export { default as CloseButton } from './CloseButton/CloseButton'
-export { default as Checkbox } from './Checkbox/Checkbox'
-export { default as CsvDownloader } from './CsvDownloader/CsvDownloader'
-export { default as CardContainer } from './Card/CardContainer'
-export { default as Header } from './Header/Header'
