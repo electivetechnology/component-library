a1e5309540f0c1f1c73c33f52fed7767d40cbe04
Chris Dixon <chris.js.dixon@gmail.com>
2021-01-13 09:53:49 +0000

    auto text

--- client/src/organisms/Form/components/FormText.tsx
+++ client/src/organisms/Form/components/FormText.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, memo, useContext, Fragment } from 'react'
 import { useFormInput } from 'organisms/Form/hooks'
 import { FormTextContainerStyled } from 'organisms/Form/styles'
 import { FormContext, InputContext } from 'organisms/Form/base'
+import FormTextArea from 'organisms/Form/components/FormTextArea'
 
 const FormText: FunctionComponent = () => {
   const { name, type, label, options } = useContext(InputContext)
@@ -18,21 +19,16 @@ const FormText: FunctionComponent = () => {
 
   return (
     <FormTextContainerStyled>
-      {options?.multiline ? (
-        <Fragment>
-          <label htmlFor={name}>{label}</label>
-          <textarea
-            id={name}
+      <Fragment>
+        <label htmlFor={name}>{label}</label>
+        {options?.multiline ? (
+          <FormTextArea
+            name={name}
             onChange={onChange}
-            onBlur={handleBlur}
+            handleBlur={handleBlur}
             value={value}
-            rows={4}
-            cols={50}
           />
-        </Fragment>
-      ) : (
-        <Fragment>
-          <label id={name}>{label}</label>
+        ) : (
           <input
             id={name}
             onChange={onChange}
@@ -40,8 +36,8 @@ const FormText: FunctionComponent = () => {
             type={type}
             value={value}
           />
-        </Fragment>
-      )}
+        )}
+      </Fragment>
     </FormTextContainerStyled>
   )
 }
