c1ff6a13b4eebee0edd7a3198290b545dd54c643
Chris Dixon <chris.js.dixon@gmail.com>
2021-01-28 12:39:16 +0000

    width

--- client/src/molecules/Columns/Columns.tsx
+++ client/src/molecules/Columns/Columns.tsx
@@ -6,41 +6,19 @@ type Props = {
   width: number
 }
 const Columns: React.FC<Props> = ({ children, width }) => {
-  const { columns, dispatcher } = useColumnsReducer(width)
+  const { columns, colWidth, dispatcher } = useColumnsReducer(width)
 
-  let childrenStyled: any = []
+  let childrenStyled: Array<any> = []
   Children.map(children, (child: any, index) => {
-    if (child) {
-      const addColumn = (
-        colspan: number,
-        fixedWidth: number,
-        display = true
-      ) => {
-        dispatcher({ type: 'add', index, colspan, fixedWidth, display })
-      }
+    const addColumn = (colspan: number, fixedWidth: number, display = true) => {
+      dispatcher({ type: 'add', index, colspan, fixedWidth, display })
+    }
+
+    child &&
       childrenStyled.push(
         cloneElement(child, { addColumn, key: index, columnIndex: index })
       )
-    }
   })
-
-  const totalSpan = columns.reduce(function (accumulator: number, column: any) {
-    const span = column.display ? column.colspan : 0
-    return accumulator + span
-  }, 0)
-
-  const totalFixed = columns.reduce(function (
-    accumulator: number,
-    column: any
-  ) {
-    const fixed = column.display ? column.fixedWidth : 0
-    return accumulator + fixed
-  },
-  0)
-
-  const widthRemaining = width - totalFixed
-
-  const colWidth = widthRemaining / totalSpan
 
   return (
     <NavContext.Provider value={{ colWidth, columns }}>
