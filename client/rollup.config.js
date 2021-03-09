ca9162129ccef7f60c3bc703edddd735b4f510b1
Chris Dixon <chris.js.dixon@gmail.com>
2021-03-09 10:56:54 +0000

    lazy

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,32 +1,38 @@
-import typescript from '@rollup/plugin-typescript'
-import pkg from './package.json'
-import resolve from '@rollup/plugin-node-resolve'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import peerExternal from 'rollup-plugin-peer-deps-external'
+import commonjs from '@rollup/plugin-commonjs';
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx']
-const input = 'src/index.ts'
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const input = "src/index.ts";
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
 
 const plugins = [
   typescript({
-    typescript: require('typescript')
+    declaration: true,
+    declarationDir: 'dist',
+    rootDir: 'src/'
   }),
-  peerDepsExternal(),
-  resolve()
-]
-const external = [
-  ...Object.keys(pkg.dependencies || {}),
-  ...Object.keys(pkg.peerDependencies || {})
+  nodeResolve(),
+  peerExternal(),
+  commonjs()
 ]
 
 export default [
   {
     input,
     output: {
-      file: pkg.module,
-      format: 'esm',
-      sourcemap: true
+      // file: pkg.module,
+      format: "esm",
+      sourcemap: true,
+      dir: 'dist',
     },
+    treeshake: false,
     plugins,
-    external
-  }
-]
+    external,
+  },
+];
