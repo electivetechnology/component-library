6ca7d2f313f5f3f6b6aff825fdd606a32d7f9952
sammyH <sammyh@recii.io>
2021-03-16 14:24:06 +0000

    update formtext field mulit no scroll

--- client/src/organisms/Form/styles.tsx
+++ client/src/organisms/Form/styles.tsx
@@ -285,6 +285,7 @@ export const TextareaStyled = styled.textarea<TextareaProps>`
   font-size: 14px;
   border-bottom: 1px solid ${theme.dividerGrey};
   font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+  overflow: hidden;
   ::-webkit-scrollbar {
     display: none;
   }
