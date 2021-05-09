05dcadafc6ea34a866af982ee2e39a3fb75ae4f0
sammyH <sammyh@recii.io>
2021-05-09 20:02:22 +0100

    update pages for mobile

--- client/src/molecules/Pages/Pages.tsx
+++ client/src/molecules/Pages/Pages.tsx
@@ -1,16 +1,17 @@
 import React, { FC, useContext } from 'react'
-import { Card } from 'atoms'
 import Page from 'molecules/Pages/Page'
 import { PageContext } from './base'
-import { CloseIconStyled, PagesContainerStyled, ProgressContainerStyled, ProgressBarStyled, ProgressIndicatorStyled, HelperTextStyled } from 'molecules/Pages/styles'
+import { CloseIconStyled, PagesContainerStyled, ProgressContainerStyled, ProgressBarStyled, ProgressIndicatorStyled, HelperTextStyled, MobileProgressNavStyled } from 'molecules/Pages/styles'
 import CloseIcon from '@material-ui/icons/Close'
-import Progress from 'atoms/Progress/Progress';
-import { theme } from 'styles/theme';
+import Progress from 'atoms/Progress/Progress'
+import { theme } from 'styles/theme'
 
 const Pages: FC = () => {
   const { back, next, progress, handleClose, currentStep, finalStep, helperText } = useContext(
     PageContext
   )
+
+  const isMobile = window.innerWidth < 760
 
   const renderProgress: any = []
 
@@ -22,14 +23,14 @@ const Pages: FC = () => {
           progressColor={theme.primaryColorValencia}
           borderColor={theme.primaryColorValencia}
         />
-      ) : startIncrement <= progress ? (
+      ) : startIncrement <= progress && !isMobile ? (
         <Progress
           label={startIncrement.toString()}
           progressColor={theme.white}
           progressFill={theme.primaryColorValencia}
           borderColor={theme.primaryColorValencia}
         />
-      ) : (
+      ) : !isMobile && (
         <Progress
           label={startIncrement.toString()}
           progressColor={theme.grey}
@@ -41,32 +42,41 @@ const Pages: FC = () => {
   }
 
   return (
-    <Card theme="primary" fullWidth noBorder>
-      <PagesContainerStyled>
-        {handleClose && (
-          <CloseIconStyled onClick={handleClose}>
-            <CloseIcon />
-          </CloseIconStyled>
+    <PagesContainerStyled>
+      {handleClose && (
+        <CloseIconStyled onClick={handleClose}>
+          <CloseIcon />
+        </CloseIconStyled>
+      )}
+      {back && <Page handleClick={back} label={'Back'} buttonTheme='secondary'/>}
+      <ProgressBarStyled isMobile>
+        {renderProgress}
+      </ProgressBarStyled>
+      <MobileProgressNavStyled>
+        {next && (
+          <Page
+            handleClick={next}
+            label={currentStep === finalStep ? 'Finish' : 'Next'}
+          />
         )}
-        {back && <Page handleClick={back} label={'Back'} />}
-        <ProgressContainerStyled>
-          <ProgressIndicatorStyled>
-            <ProgressBarStyled>
-              {renderProgress}
-            </ProgressBarStyled>
-            {next && (
-              <Page
-                handleClick={next}
-                label={currentStep === finalStep ? 'Finish' : 'Next'}
-              />
-            )}
-          </ProgressIndicatorStyled>
-          <HelperTextStyled>
-            {helperText}
-          </HelperTextStyled>
-        </ProgressContainerStyled>
-      </PagesContainerStyled>
-    </Card>
+      </MobileProgressNavStyled>
+      <ProgressContainerStyled>
+        <ProgressIndicatorStyled>
+          <ProgressBarStyled>
+            {renderProgress}
+          </ProgressBarStyled>
+          {next && (
+            <Page
+              handleClick={next}
+              label={currentStep === finalStep ? 'Finish' : 'Next'}
+            />
+          )}
+        </ProgressIndicatorStyled>
+        <HelperTextStyled>
+          {helperText}
+        </HelperTextStyled>
+      </ProgressContainerStyled>
+    </PagesContainerStyled>
   )
 }
 
