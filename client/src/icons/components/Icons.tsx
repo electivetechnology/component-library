5a90b87e4e660b065fc5c068757d4745436fdabd
sammyH <sammyh@recii.io>
2021-05-12 13:30:44 +0100

    add logo svg to folder

---
+++ client/src/icons/components/Icons.tsx
@@ -0,0 +1,16 @@
+import React, { Children, FunctionComponent, useEffect, useState } from 'react'
+
+type Props = {
+
+}
+
+const Icon: FunctionComponent<Props> = ({
+  
+}) => {
+
+  return (
+    <div>icons</div>
+  )
+}
+
+export default Icon
---
+++ client/src/icons/stories/Icon.stories.tsx
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react'
+import Icon from 'icons/components/Icons'
+
+// const Template = (args: any) => <Form {...args} handleUpdate={handleUpdate} />
+
+// FormCheckbox
+// export const Checkbox: any = Template.bind({})
+
+// Checkbox.args = {
+//   handleUpdate,
+//   children: (
+//     <Fragment>
+//       <FormInput
+//         label='Checkbox'
+//         name='checkbox'
+//         value={true}
+//         type='checkbox'
+//       />
+//       <FormInput
+//         label='Checkbox'
+//         name='checkbox'
+//         value={true}
+//         type='checkbox'
+//         required={true}
+//       />
+//     </Fragment>
+//   )
+// }
+
+export default {
+  title: 'organisms/Icon/Icons',
+  component: Icon
+}
