b586893e35c9feb43d7dffe47ad223196b409198
Chris Dixon <chris.js.dixon@gmail.com>
2021-06-09 17:17:53 +0100

    index

--- client/src/molecules/Select/index.ts
+++ client/src/molecules/Select/index.ts
@@ -1,3 +1,4 @@
 export { default as Select } from 'molecules/Select/Select'
 export { default as Option } from 'molecules/Select/Option'
+export { default as OptionHeader } from 'molecules/Select/OptionHeader'
 export { SelectContext } from 'molecules/Select/base'
