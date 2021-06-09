e13dbe822ad4828f7af2073a530e7e7f27b32569
Chris Dixon <chris.js.dixon@gmail.com>
2021-06-09 20:31:16 +0100

    required

--- client/src/molecules/Select/Select.tsx
+++ client/src/molecules/Select/Select.tsx
@@ -1,13 +1,20 @@
 import React, { useContext, FC, useState, useEffect } from 'react'
 import { OptionType, SelectContext } from './base'
 import { SelectContainerStyled, SelectStyled } from 'molecules/Select/styles'
+import Option from 'molecules/Select/Option'
 
 type Props = {
   initialValue: OptionType
   onChange: Function
+  required: boolean
 }
 
-const Select: FC<Props> = ({ initialValue, onChange, children }) => {
+const Select: FC<Props> = ({
+  initialValue,
+  onChange,
+  children,
+  required = false
+}) => {
   const defaultSelected = initialValue ? initialValue : { label: '', value: '' }
   const [selected, setSelected] = useState(defaultSelected)
   const [showOptions, setShowOptions] = useState(false)
@@ -37,6 +44,7 @@ const Select: FC<Props> = ({ initialValue, onChange, children }) => {
     >
       <SelectContainerStyled onClick={handleActive}>
         <SelectStyled>{label ? label : 'None'}</SelectStyled>
+        {showOptions && required && <Option label='' value='' />}
         {showOptions && children}
       </SelectContainerStyled>
     </SelectContext.Provider>
