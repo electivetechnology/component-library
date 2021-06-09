1d0f4436d88feb6c9bc75eeca5353b767f1b948b
Chris Dixon <chris.js.dixon@gmail.com>
2021-06-09 13:33:46 +0100

    select

---
+++ client/src/molecules/Select/Option.tsx
@@ -0,0 +1,26 @@
+import React, { FC, useContext } from 'react'
+import { SelectContext } from './base'
+import { OptionStyled } from 'molecules/Select/styles'
+
+type Props = {
+  label: string
+  value: string
+}
+const Select: FC<Props> = ({ label, value }) => {
+  const {
+    selected: { value: selectedValue },
+    setSelected
+  } = useContext(SelectContext)
+
+  const handleSelect = () => {
+    setSelected({ label, value })
+  }
+
+  return (
+    <OptionStyled onClick={handleSelect} active={selectedValue === value}>
+      {label} - {value}
+    </OptionStyled>
+  )
+}
+
+export default Select
---
+++ client/src/molecules/Select/index.ts
@@ -0,0 +1,3 @@
+export { default as Select } from 'molecules/Select/Select'
+export { default as Option } from 'molecules/Select/Option'
+export { SelectContext } from 'molecules/Select/base'
