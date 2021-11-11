ecd87c1aad05aa8fe50fddf71e6273f91c3275ba
sammyh <sammy_nlh@hotmail.com>
2021-11-11 17:12:14 +0000

    refactor divider

--- client/src/atoms/Divider/Divider.tsx
+++ client/src/atoms/Divider/Divider.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import { DividerStyled } from './styles'
 
-const Divider: React.FC = () => {
-
-  return (
-    <DividerStyled />
-  )
-}
+const Divider: React.FC = () => <DividerStyled />
 
 export default Divider
