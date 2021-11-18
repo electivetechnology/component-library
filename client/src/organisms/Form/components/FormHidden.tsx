546c5d14b528e2af33834cd729c30fe41596bb2e
Chris Dixon <chris.js.dixon@gmail.com>
2021-11-18 17:09:57 +0000

    hidden form

--- client/src/organisms/Form/components/FormHidden.tsx
+++ client/src/organisms/Form/components/FormHidden.tsx
@@ -1,12 +1,18 @@
-import React, {forwardRef} from 'react'
+import React, { forwardRef, FunctionComponent, memo, useContext } from 'react'
 import { useFormSave } from 'organisms/Form/hooks'
+import { FormContext } from 'organisms/Form/base'
+import { Font } from 'atoms'
+import { theme } from 'styles/theme'
 
 type Props =  {
   handleSave: Function
 }
 
-export const FormHidden = forwardRef<any, Props>(({ handleSave }, ref) => {
+const FormHidden = forwardRef<any, Props>(({ handleSave }, ref) => {
   const { handleAction } = useFormSave(handleSave)
 
   return <button ref={ref} onClick={handleAction} style={{ visibility: 'hidden' }}/>
 })
+
+export default FormHidden
+
