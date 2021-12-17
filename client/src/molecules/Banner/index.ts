bd9eb1b2a573865804aa57a68deeaf7258131084
Chris Dixon <chris.js.dixon@gmail.com>
2021-12-17 17:24:55 +0000

    materail

--- client/src/molecules/Banner/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Banner } from 'molecules/Banner/Banner'
-export { default as BannerContent } from 'molecules/Banner/BannerContent'
-export { BannerContext } from 'molecules/Banner/base'
-
