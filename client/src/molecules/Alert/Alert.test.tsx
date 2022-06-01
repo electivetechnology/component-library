76f77fdc68ac9dd375af8462a7fc5736e6ed4545
Chris Dixon <chris.js.dixon@gmail.com>
2022-06-01 16:51:18 +0100

    remove redundant

--- client/src/molecules/Alert/Alert.test.tsx
+++ client/src/molecules/Alert/Alert.test.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Alert from 'molecules/Alert/Alert'
-import AlertMessage from 'molecules/Alert/AlertMessage'
-import AlertContent from 'molecules/Alert/AlertContent'
 import { successContextMock } from 'molecules/Alert/mocks'
 import { AlertContext } from 'molecules/Alert/base'
 
@@ -16,22 +14,4 @@ test('renders Alert', () => {
     </AlertContext.Provider>
   )
   expect(getByTestId('Alert')).toBeDefined
-})
-
-test('renders AlertContent', () => {
-  const { getByTestId } = render(
-    <AlertContext.Provider value={successContextMock}>
-      <AlertContent />
-    </AlertContext.Provider>
-  )
-  expect(getByTestId('AlertContent')).toBeDefined
-})
-
-test('renders AlertMessage', () => {
-  const { getByTestId } = render(
-    <AlertContext.Provider value={successContextMock}>
-      <AlertMessage />
-    </AlertContext.Provider>
-  )
-  expect(getByTestId('AlertMessage')).toBeDefined
 })
